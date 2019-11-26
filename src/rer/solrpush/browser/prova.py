73209b4b54c01b4c117ee75b3b23d2640092e381
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-11-26 14:48:58 +0100

    removed unused files

--- src/rer/solrpush/browser/prova.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from Products.Five import BrowserView
-from rer.solrpush.solr import search
-
-
-class View(BrowserView):
-    def __call__(self):
-        search(self.request.form)
