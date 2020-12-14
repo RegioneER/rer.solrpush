769869a16177977a93387db3a3beaff73bc01389
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-12-14 13:59:14 +0100

    Back to development: 0.2.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="rer.solrpush",
-    version="0.2.2",
+    version="0.2.3.dev0",
     description="Prodotto per Regione Emilia-Romagna relativo all'indicizzazione dei contenuti con solr",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
