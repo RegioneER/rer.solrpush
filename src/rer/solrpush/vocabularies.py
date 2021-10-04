2a65854dd255180085bd0b64a5cc724325d6567f
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-10-04 17:32:01 +0200

    Add new criteria: solr_portal_types to select a list of portal_types indexed on SOLR

--- src/rer/solrpush/vocabularies.py
+++ src/rer/solrpush/vocabularies.py
@@ -5,19 +5,17 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 
-@implementer(IVocabularyFactory)
-class AvailableSitesVocabularyFactory(object):
-    @property
-    def terms(self):
+class FacetsVocabulary(object):
+    def get_terms(self):
         solr_results = search(
             query={"*": "*", "b_size": 1},
             fl="UID",
             facets=True,
-            facet_fields="site_name",
+            facet_fields=self.facet_field,
         )
         if isinstance(solr_results, dict) and solr_results.get("error", False):
             return []
-        facets = solr_results.facets["facet_fields"].get("site_name", [])
+        facets = solr_results.facets["facet_fields"].get(self.facet_field, [])
         if not facets:
             return []
         terms = []
@@ -29,38 +27,24 @@ class AvailableSitesVocabularyFactory(object):
         return terms
 
     def __call__(self, context):
-        return SimpleVocabulary(self.terms)
+        return SimpleVocabulary(self.get_terms(), swallow_duplicates=True)
 
 
 @implementer(IVocabularyFactory)
-class AvailableSubjectsVocabularyFactory(object):
-    @property
-    def terms(self):
-        solr_results = search(
-            query={"*": "*", "b_size": 1},
-            fl="UID",
-            facets=True,
-            facet_fields="Subject",
-        )
-        if isinstance(solr_results, dict) and solr_results.get("error", False):
-            return []
-        facets = solr_results.facets["facet_fields"].get("Subject", [])
-        if not facets:
-            return []
-        terms = []
-        for facet in facets:
-            for key in facet.keys():
-                terms.append(
-                    SimpleTerm(value=key, token=key.encode("utf-8"), title=key)
-                )
-        return terms
-
-    def __call__(self, context):
-        # we use swallow_duplicates to avoid errors.
-        # SimpleVocabulary init in zope.schema 4.5.0 strips "strange" chars
-        # In python3 probably this is unnecessary
-        return SimpleVocabulary(terms=self.terms, swallow_duplicates=True)
+class AvailableSitesVocabularyFactory(FacetsVocabulary):
+    facet_field = "site_name"
 
 
+@implementer(IVocabularyFactory)
+class AvailableSubjectsVocabularyFactory(FacetsVocabulary):
+    facet_field = "Subject"
+
+
+@implementer(IVocabularyFactory)
+class AvailablePortalTypesVocabularyFactory(FacetsVocabulary):
+    facet_field = "portal_type"
+
+
+AvailablePortalTypesVocabulary = AvailablePortalTypesVocabularyFactory()
 AvailableSitesVocabulary = AvailableSitesVocabularyFactory()
 AvailableSubjectsVocabulary = AvailableSubjectsVocabularyFactory()
