4a194d36e9065d8ab080e56835905c7b95e51daf
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-04-18 10:57:01 +0200

    blacked

--- src/rer/solrpush/restapi/deserializer/controlpanel.py
+++ src/rer/solrpush/restapi/deserializer/controlpanel.py
@@ -1,4 +1,6 @@
-from plone.restapi.deserializer.controlpanels import ControlpanelDeserializeFromJson
+from plone.restapi.deserializer.controlpanels import (
+    ControlpanelDeserializeFromJson,
+)
 from plone.restapi.interfaces import IDeserializeFromJson
 from rer.solrpush.interfaces import IRERSolrpushRestapiControlpanel
 from rer.solrpush.utils import init_solr_push
